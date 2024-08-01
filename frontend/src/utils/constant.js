3858f102eb6d13d3b3cedb2b7f31fe7a7bdf8f4d
napharsh <harsh.goel@napworks.in>
2024-08-01 17:55:22 +0530

    changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-import.meta.env.BASE_URL
+const VITE_URL = import.meta.env.VITE_URL;
 
-export const USER_API_END_POINT=`${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT=`${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT=`${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT=`${BASE_URL}/api/v1/company`;
+export const USER_API_END_POINT = `${VITE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${VITE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${VITE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${VITE_URL}/api/v1/company`;
