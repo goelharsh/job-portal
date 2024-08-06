196cd4c9d86dc7b8fa0aada82bf42b66aee16a1d
napharsh <harsh.goel@napworks.in>
2024-08-06 12:54:59 +0530

    Adding default route

--- backend/index.js
+++ backend/index.js
@@ -14,27 +14,29 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
     origin: 'http://localhost:5173',
-    credentials:true
-}
+    credentials: true
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+// Default route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
